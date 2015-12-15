acf195ca50158f1cab539dd98b7c116a7e0f5ff6
Mike Rys <michaelrys@hotmail.com>
2015-12-14 22:28:10 -0800

    Added TweetAnalysis Samples and aligned sample data folder structure to account sample data folder structure

---
+++ Examples/TweetAnalysis/TweetAnalysisClass/TweetAnalysis.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Linq;
+
+// TweetAnalysis Assembly
+// Show the use of a U-SQL user-defined function (UDF)
+//
+// Register this assembly at master.TweetAnalysis either using Register Assembly on TweetAnalysisClass project in Visual Studio
+// or by compiling the assembly, uploading it (if you want to run it in Azure), and registering it with REGISTER ASSEMBLY U-SQL script.
+//
+namespace TweetAnalysis
+{
+    public class Udfs
+    {
+        // SqlArray<string> get_mentions(string tweet)
+        //
+        // Returns a U-SQL array of string containing the twitter handles that were mentioned inside the tweet.
+        //
+        public static SqlArray<string> get_mentions(string tweet)
+        {
+            return new SqlArray<string>(
+                tweet.Split(new char[] { ' ', ',', '.', ':', '!', ';', '"', '“' }).Where(x => x.StartsWith("@"))
+                );
+        }
+    }
+}
