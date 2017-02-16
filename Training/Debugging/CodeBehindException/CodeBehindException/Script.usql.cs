c4fedb3c201fbc1c5eb0775fdbf698c14a64fc20
saveenr <saveenr@live.com>
2017-02-16 15:48:51 -0800

    Added additional Training Samples

---
+++ Training/Debugging/CodeBehindException/CodeBehindException/Script.usql.cs
@@ -0,0 +1,18 @@
+﻿namespace CodeBehindException
+{
+    public static class Helpers
+    {
+        public static string NormalizeAuthor(string author)
+        {
+            if (author == "mrys")
+            {
+                throw new System.ArgumentException("Unsupported author");
+            }
+
+            author = author.Trim();
+            author = author.ToUpperInvariant();
+            return author;
+        }
+    }
+
+}
---
+++ Training/Debugging/MVA_UDAgg/MVA_UDAgg/Script.usql.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Analytics.Interfaces;
+
+namespace MVA_UDAgg
+{
+    public class MySum : IAggregate<int, int, long>
+    {
+        long total;
+
+        public override void Init()
+        {
+            total = 0;
+        }
+
+        public override void Accumulate(int i1, int i2)
+        {
+            total += (i1*i1);
+        }
+
+        public override long Terminate()
+        {
+            return total;
+        }
+    }
+}
