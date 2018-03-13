7660886906cedccf46dd94b01592cd8a083f5f0d
Syed Hassaan Ahmed <hassaan.brix@gmail.com>
2018-03-13 19:49:39 +0100

    Publish example of splitting geotiff rasters

---
+++ Examples/SplitGeoTiffRasters/SplitTiff/SplitTiffExtractor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Microsoft.Analytics.Interfaces;
+
+namespace SplitTiff
+{
+    [SqlUserDefinedExtractor(AtomicFileProcessing = true)]
+    public class SplitTiffExtractor : IExtractor
+    {
+        private readonly int _chunkWidth;
+        private readonly int _chunkHeight;
+
+        public SplitTiffExtractor(int chunkWidth, int chunkHeight)
+        {
+            _chunkWidth = chunkWidth;
+            _chunkHeight = chunkHeight;
+        }
+
+        public override IEnumerable<IRow> Extract(IUnstructuredReader input, IUpdatableRow output)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                // LibTiff.Net internally calls something ADLA stream doesn't support
+                input.BaseStream.CopyTo(memoryStream);
+                memoryStream.Position = 0;
+
+                var tiffArray = SplitTiff.Split(memoryStream, _chunkWidth, _chunkHeight);
+
+                foreach (var tiff in tiffArray)
+                {
+                    output.Set("tiff", tiff);
+                    yield return output.AsReadOnly();
+                }
+            }
+        }
+    }
+}
---
+++ Examples/SplitGeoTiffRasters/SplitTiff/ZipOutputter.cs
@@ -0,0 +1,52 @@
+﻿using System.IO;
+using System.IO.Compression;
+using Microsoft.Analytics.Interfaces;
+
+namespace SplitTiff
+{
+    [SqlUserDefinedOutputter(AtomicFileProcessing = false)]
+    public class ZipOutputter : IOutputter
+    {
+        private readonly string _filePattern;
+        private int _fileCount;
+
+        private readonly MemoryStream _memoryStream = new MemoryStream();
+        private readonly ZipArchive _archive;
+        private Stream _outputStream;
+
+        public ZipOutputter(string filePattern)
+        {
+            _filePattern = filePattern;
+            _archive = new ZipArchive(_memoryStream, ZipArchiveMode.Create, true);
+        }
+
+        public override void Output(IRow input, IUnstructuredWriter output)
+        {   
+            var file = input.Get<byte[]>(0);
+            _outputStream = output.BaseStream;
+
+            var entry = _archive.CreateEntry(string.Format(_filePattern, _fileCount++),
+                CompressionLevel.NoCompression);
+
+            using (var ms = new MemoryStream(file))
+            using (var entryStream = entry.Open())
+            {
+                ms.CopyTo(entryStream);
+            }
+        }
+
+        public override void Close()
+        {
+            // ZipArchive has to be disposed to make it write its content to its underlying stream
+            _archive.Dispose();
+
+            using (var writer = new BinaryWriter(_outputStream))
+            {
+                writer.Write(_memoryStream.ToArray());
+                writer.Flush();
+            }
+
+            _memoryStream.Dispose();
+        }
+    }
+}
