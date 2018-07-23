020d4b0bdda6828287a08f2d65412fe510359926
yanancai <yanacai@microsoft.com>
2018-07-23 20:00:15 +0800

    add PowerShell script for U-SQL file uploading from U-SQL build output.

---
+++ Examples/CICD/Deployment/USQLScriptUpload.ps1
@@ -0,0 +1,53 @@
+<#
+    This script can be used to upload U-SQL files to ADLS with given U-SQL project build output(.usqlpack file).
+    This will unzip the U-SQL project build output, and upload all scripts to ADLS one-by-one.
+
+    Example :
+        FileUpload.ps1 -ADLSName "myadlsaccount" -ArtifactsRoot "C:\USQLProject\bin\debug\"
+#>
+
+param(
+    [Parameter(Mandatory=$true)][string]$ADLSName, # ADLS account name to upload U-SQL scripts
+    [Parameter(Mandatory=$true)][string]$ArtifactsRoot, # Root folder of U-SQL project build output
+    [Parameter(Mandatory=$false)][string]$DesitinationFolder = "USQLScriptSource" # Desitination folder in ADLS
+)
+
+Function UploadResources()
+{
+    Write-Host "************************************************************************"
+    Write-Host "Uploading files to $ADLSName"
+    Write-Host "***********************************************************************"
+
+    $usqlScripts = GetUsqlFiles
+
+    $files = @(get-childitem $usqlScripts -recurse)
+    foreach($file in $files)
+    {
+        Write-Host "Uploading file: $($file.Name)"
+        Import-AzureRmDataLakeStoreItem -AccountName $ADLSName -Path $file.FullName -Destination "/$(Join-Path $DesitinationFolder $file)" -Force
+    }
+}
+
+function Unzip($USQLPackfile, $UnzipOutput)
+{
+    $USQLPackfileZip = Rename-Item -Path $USQLPackfile -NewName $([System.IO.Path]::ChangeExtension($USQLPackfile, ".zip")) -Force -PassThru
+    Expand-Archive -Path $USQLPackfileZip -DestinationPath $UnzipOutput -Force
+    Rename-Item -Path $USQLPackfileZip -NewName $([System.IO.Path]::ChangeExtension($USQLPackfileZip, ".usqlpack")) -Force
+}
+
+Function GetUsqlFiles()
+{
+
+    $USQLPackfiles = Get-ChildItem -Path $ArtifactsRoot -Include *.usqlpack -File -Recurse -ErrorAction SilentlyContinue
+
+    $UnzipOutput = Join-Path $ArtifactsRoot -ChildPath "UnzipUSQLScripts"
+
+    foreach ($USQLPackfile in $USQLPackfiles)
+    {
+        Unzip $USQLPackfile $UnzipOutput
+    }
+
+    return Get-ChildItem -Path $UnzipOutput -Include *.usql -File -Recurse -ErrorAction SilentlyContinue
+}
+
+UploadResources
